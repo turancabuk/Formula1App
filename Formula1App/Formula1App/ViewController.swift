7ea5f6e4f59bd570ebbf68755c504c9ce8c9e076
turancabuk <turan6677@gmail.com>
2023-03-12 19:28:04 +0300

    the project has been created

---
+++ Formula1App/Formula1App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Formula1App
+//
+//  Created by Turan Çabuk on 12.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
